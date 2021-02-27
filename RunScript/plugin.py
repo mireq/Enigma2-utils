2636604f6b66530ef133b67580caded12309160e
Miroslav Bendík <miroslav.bendik@gmail.com>
2021-02-27 11:23:51 +0100

    Added support for redirected stdout/stderr

--- RunScript/plugin.py
+++ RunScript/plugin.py
@@ -1,11 +1,25 @@
 from Plugins.Plugin import PluginDescriptor
+from contextlib import contextmanager
 from threading import Thread
 import errno
 import os
 import posix
-import traceback
-from threading import Thread
 import sys
+import traceback
+
+
+@contextmanager
+def redirect_stdout(new_target):
+	if new_target is None:
+		yield new_target
+	else:
+		old_stdout, old_stderr = sys.stdout, sys.stderr
+
+		try:
+			sys.stdout, sys.stderr = new_target, new_target
+			yield new_target
+		finally:
+			sys.stdout, sys.stderr = old_stdout, old_stderr
 
 
 def run_server():
@@ -15,19 +29,31 @@ def run_server():
 			fifo = open('/tmp/run_script.fifo', 'r')
 		except IOError:
 			return
+
 		commands = fifo.read().strip().split('\n')
+
 		try:
-			for command in commands:
-				sys.stderr.write("RunScript running: %s\n" % command)
-				if command == 'quit':
-					return
-				if not command:
-					continue
-				with open(command, 'r') as script_fp:
-					script = script_fp.read()
-				exec(script)
-		except Exception as e:
-			traceback.print_exc()
+			output = posix.open('/tmp/run_script_output.fifo', posix.O_WRONLY | posix.O_NONBLOCK)
+			output = os.fdopen(output, 'w')
+		except OSError:
+			output = None
+
+		with redirect_stdout(output):
+			try:
+				for command in commands:
+					sys.stderr.write("RunScript running: %s\n" % command)
+					if command == 'quit':
+						return
+					if not command:
+						continue
+					with open(command, 'r') as script_fp:
+						script = script_fp.read()
+					exec(script)
+			except Exception as e:
+				traceback.print_exc()
+
+		if output is not None:
+			output.close()
 
 
 def autostart(reason, **kwargs):
@@ -35,6 +61,10 @@ def autostart(reason, **kwargs):
 	if reason == 0:
 		try:
 			os.mkfifo('/tmp/run_script.fifo', 0o666)
+		except OSError:
+			pass
+		try:
+			os.mkfifo('/tmp/run_script_output.fifo', 0o666)
 		except OSError:
 			pass
 		thread = Thread(target=run_server)
@@ -46,11 +76,6 @@ def autostart(reason, **kwargs):
 			posix.close(fp)
 		except OSError:
 			pass
-
-
-
-
-
 
 
 def Plugins(**kwargs): 
